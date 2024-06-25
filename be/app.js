13699d86a6336045e45e6f3bdd5f2ec910216747
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-06-25 09:49:00 +0700

    change

--- be/app.js
+++ be/app.js
@@ -11,7 +11,7 @@ const authorizationAdmin = require('./middlewares/authorizationAdmin');
 const LoginController = require('./controllers/loginController');
 
 app.use(cors({
-  origin: 'http://192.168.202.166:5173',
+  origin: ['http://192.168.202.166:5173', 'http://localhost:5173'],
   credentials: true
 }));
 
