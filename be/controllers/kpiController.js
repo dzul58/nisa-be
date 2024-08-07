fd2ddf171ef8b0d2932da79ea3444342c8bab9ca
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-08-07 14:15:58 +0700

    added getAllKpi

--- be/controllers/kpiController.js
+++ be/controllers/kpiController.js
@@ -1,7 +1,13 @@
 const poolNisa = require('../config/configNisa')
 
 class KpiController {
-
+static async getAllCsKpi (req, res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
 }
 
 module.exports = KpiController;
